135f478fd46d456a09bfc2128fd76a74c76ec23b
Ryan Mitchell <Ryan.E.Mitchell@gmail.com>
2011-05-04 00:30:30 -0400

    Updating svg-edit code with modified SVG to PNG converting functions

--- saveSVG.php
+++ saveSVG.php
@@ -2,12 +2,12 @@
 <HEAD>
 <?php
 $svgArray = array();
-$svgArray = explode(';;', $_POST['svgArray']);
-$_POST['pid'] = 2;
+$svgArray = explode(';;', $_GET['svgArray']);
+$_GET['pid'] = 2;
 $name = "shape";
 $server = "javasaurcom.fatcowmysql.com";
 $username = "javasaur";
-$password = "i<3tyler";
+$password = "password";
 $db = "javasaur";
 $conn = mysql_connect($server, $username, $password, $db);
 mysql_select_db($db);
@@ -15,7 +15,7 @@ mysql_select_db($db);
 
 foreach($svgArray as $i => $svg){
     if($svg != ""){
-        $query = "INSERT INTO elements (pid, name, url) VALUES (".$_POST['pid'].", \"".str_replace (" " , "", $_POST[$i])."\", \"".$svg."\");";
+        $query = "INSERT INTO elements (pid, name, url) VALUES (".$_GET['pid'].", \"".str_replace (" " , "", $_GET[$i])."\", \"".$svg."\");";
         print "<p>".$query."<p>";
         mysql_query($query);
     }
